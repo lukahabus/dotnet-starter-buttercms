9cc42b1fa8f0d19c484a113bc4804e368ca9ef36
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-03-25 14:10:24 +0100

    Initial commit

---
+++ ButterCMS.Starter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ButterCMS.Starter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
