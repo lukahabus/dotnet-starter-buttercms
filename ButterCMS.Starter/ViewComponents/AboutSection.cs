8389e16e126f15803183ef59b1b2cee2b65699bf
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-03-28 12:46:12 +0200

    feat: static about section

---
+++ ButterCMS.Starter/ViewComponents/AboutSection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ButterCMS.Starter.ViewComponents
+{
+    public class AboutSection : ViewComponent
+    {
+        public AboutSection()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
