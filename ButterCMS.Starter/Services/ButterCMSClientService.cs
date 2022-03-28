86002f5c417e43603972784349b31910b11e74d5
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-03-28 18:25:33 +0200

    feat: BlogPostTile souring from CMS

---
+++ ButterCMS.Starter/Services/ButterCMSClientService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace ButterCMS.Starter.Services
+{
+    public class ButterCMSClientService
+    {
+        private readonly string apiKey;
+
+        public ButterCMSClientService(IConfiguration configuration)
+        {
+            this.apiKey = configuration["ButterCMS:APIKey"];
+        }
+
+        public ButterCMSClient GetClient() => new ButterCMSClient(this.apiKey);
+    }
+}
--- ButterCMS.Starter/ViewComponents/BlogSection.cs
+++ ButterCMS.Starter/ViewComponents/BlogSection.cs
@@ -1,17 +1,33 @@
 ﻿using System;
+using System.Threading.Tasks;
+using ButterCMS.Starter.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace ButterCMS.Starter.ViewComponents
 {
     public class BlogSection : ViewComponent
     {
-        public BlogSection()
+        private readonly ButterCMSClient client;
+
+        private readonly int postsCount;
+
+        public BlogSection(ButterCMSClientService clientService, IConfiguration configuration) 
         {
+            this.client = clientService.GetClient();
+            this.postsCount = Int32.Parse(configuration["BlogSection:PostsCount"]);
         }
 
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var postsResponse = await this.GetBlogPosts(1, this.postsCount);
+
+            return View(postsResponse.Data);
+        }
+
+        private async Task<ButterCMS.Models.PostsResponse> GetBlogPosts(int page, int pageSize)
+        {
+            return await this.client.ListPostsAsync(page, pageSize);
         }
     }
 }
