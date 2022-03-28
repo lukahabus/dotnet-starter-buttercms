e9d71e080a8c51547ab59ad9c087bad6e606f58a
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-03-28 12:41:56 +0200

    feat: static hero section

---
+++ ButterCMS.Starter/ViewComponents/HeroSection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ButterCMS.Starter.ViewComponents
+{
+    public class HeroSection : ViewComponent
+    {
+        public HeroSection()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
