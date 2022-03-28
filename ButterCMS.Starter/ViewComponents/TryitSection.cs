8cddc0d990622504d547e3f145dfe6a6a742a416
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-03-28 12:53:15 +0200

    feat: section tryit section

---
+++ ButterCMS.Starter/ViewComponents/TryitSection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ButterCMS.Starter.ViewComponents
+{
+    public class TryitSection : ViewComponent
+    {
+        public TryitSection()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
