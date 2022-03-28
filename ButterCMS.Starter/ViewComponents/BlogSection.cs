7970db60e5bd0ac07e7cf063ae1bb07fa9d4990c
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-03-28 13:00:40 +0200

    feat: static blog section

---
+++ ButterCMS.Starter/ViewComponents/BlogSection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ButterCMS.Starter.ViewComponents
+{
+    public class BlogSection : ViewComponent
+    {
+        public BlogSection()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
