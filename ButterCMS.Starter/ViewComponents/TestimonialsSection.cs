1fd9743981038ed432c8c8e215fba024f3572e4e
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-03-28 12:55:24 +0200

    feat: static testimonials section

---
+++ ButterCMS.Starter/ViewComponents/TestimonialsSection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ButterCMS.Starter.ViewComponents
+{
+    public class TestimonialsSection : ViewComponent
+    {
+        public TestimonialsSection()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
