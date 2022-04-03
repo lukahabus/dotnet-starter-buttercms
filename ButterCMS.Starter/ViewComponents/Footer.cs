c8d077999a8a53024cb673ba1bdb479025823261
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-04-03 18:38:49 +0200

    feat(footer): fetch data from cms

---
+++ ButterCMS.Starter/ViewComponents/Footer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using ButterCMS.Starter.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ButterCMS.Starter.ViewComponents
+{
+    public class Footer : ViewComponent
+    {
+        private readonly NavigationService navigationService;
+
+        public Footer(NavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await this.navigationService.GetNavigationItems());
+        }
+    }
+}
